8f4220aeb4f23e56acb83d1835e0fb6d2a5e44de
Anastasia <zharkovaanasta@yandex.ru>
2023-08-26 23:00:22 +0300

    фикс ошибки

--- src/main/java/ru/yandex/practicum/filmorate/exeption/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exeption/ValidationException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exeption;
 
-public class ValidationException extends RuntimeException{
+public class ValidationException extends RuntimeException {
     public ValidationException(final String message) {
         super(message);
     }
